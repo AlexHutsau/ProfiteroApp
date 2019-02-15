a775cca359717ae338002d181e330355b3ff5a4c
Aliaksander Hutsau <aleksan9ep@gmail.com>
2019-02-15 20:41:06 +0300

    adding squared number programm.

--- test.rb
+++ test.rb
@@ -1,1 +1,10 @@
+puts "Hello there! Please enter a number to square: "
+number = gets.chomp.to_i
 
+def square(number)
+    squared = number * number
+    return squared.to_s
+end
+
+puts "Your number is: " + number.to_s
+puts "Your number squared is: " + square(number)
